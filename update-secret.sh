7b482af8418de03973fdd7ebb51f14d888b4882d
Bodo Junglas <junglas@objectcode.de>
2022-06-14 14:50:08 +0200

    Fix command

--- update-secret.sh
+++ update-secret.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 kubectl delete secret $ECR_SECRET -n $NAMESPACE
-kubectl create secret docker-secret $ECR_SECRET --docker-server=$AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com --docker-username=AWS --docker-password=$(aws ecr get-login-password) --namespace $NAMESPACE
+kubectl create secret docker-registry $ECR_SECRET --docker-server=$AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com --docker-username=AWS --docker-password=$(aws ecr get-login-password) --namespace $NAMESPACE
